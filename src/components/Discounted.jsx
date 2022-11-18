8875e4afecff4714514e23b118d7bad2f071a155
David <davidmollera@hotmail.com>
2022-11-17 22:01:05 -0500

    working on the discounted component

--- src/components/Discounted.jsx
+++ src/components/Discounted.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { books } from "../data";
+import Book from "./ui/book";
 
 const Discounted = () => {
-    return (
-       <section id="recent">
-        
-       </section>
-    );
-}
+  return (
+    <section id="recent">
+      <div className="container">
+        <div className="row">
+          <h2 className="section__title">
+            Discounted <span className="purple">Books</span>
+          </h2>
+          <div className="books">
+            {books.map((book) => <Book book={book} key={book.id} />)}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
 export default Discounted;
