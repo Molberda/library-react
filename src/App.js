a499502e2c5fd49c06b899fe0d405564450c9b6e
David <davidmollera@hotmail.com>
2022-11-19 18:53:16 -0500

    created a cart page and linked it to the cart button, also added the route to the cart page and started adding html code to it

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Nav from "./components/Nav";
 import Books from "./Pages/Books";
 import { books } from "./data.js"
 import BookInfo from "./Pages/BookInfo";
+import Cart from "./Pages/Cart";
 
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<Home/>} />
           <Route path="/books" element={<Books books={books} /> }/>
           <Route path="/books/:id" element={<BookInfo books={books}/> }/>
+          <Route path="/cart" element={<Cart /> }/>
         </Routes>
         <Footer/>
       </div>
