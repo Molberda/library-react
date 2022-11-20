da699cde37c3ab3f78dfeea4bdc016f932720a7c
David <davidmollera@hotmail.com>
2022-11-19 22:57:19 -0500

    continued working on the addToCart function

--- src/App.js
+++ src/App.js
@@ -3,44 +3,51 @@ import Footer from "./components/Footer";
 import Home from "./Pages/Home";
 import Nav from "./components/Nav";
 import Books from "./Pages/Books";
-import { books } from "./data.js"
+import { books } from "./data.js";
 import BookInfo from "./Pages/BookInfo";
 import Cart from "./Pages/Cart";
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [cart, setCart] = useState([])
+  const [cart, setCart] = useState([]);
 
-function addToCart(book){
-  const dupeItem = cart.find(item => +item.id === book.id)
-  if(dupeItem){
-    setCart(cart.map(item => {
-      if (+item.id === +dupeItem.id){
-        return {
-          ...item,
-          quantity: item.quantity +1
-        }
-      }
-    }))
+  function addToCart(book) {
+    const dupeItem = cart.find((item) => +item.id === book.id);
+    if (dupeItem) {
+      setCart(
+        cart.map((item) => {
+          if (+item.id === +dupeItem.id) {
+            return {
+              ...item,
+              quantity: item.quantity + 1,
+            };
+          } else {
+            return item;
+          }
+        })
+      );
+    } else {
+      setCart([...cart, { ...book, quantity: 1 }]);
+    }
   }
 
-  setCart([...cart, {...book, quantity: 1}])
-}
-
-useEffect(() => {
-  console.log(cart)
-},[cart])
+  useEffect(() => {
+    console.log(cart);
+  }, [cart]);
   return (
     <Router>
       <div className="App">
         <Nav />
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/books" element={<Books books={books} /> }/>
-          <Route path="/books/:id" element={<BookInfo books={books} addToCart={addToCart}/> }/>
-          <Route path="/cart" element={<Cart /> }/>
+          <Route path="/" element={<Home />} />
+          <Route path="/books" element={<Books books={books} />} />
+          <Route
+            path="/books/:id"
+            element={<BookInfo books={books} addToCart={addToCart} />}
+          />
+          <Route path="/cart" element={<Cart />} />
         </Routes>
-        <Footer/>
+        <Footer />
       </div>
     </Router>
   );
