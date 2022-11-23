c89bb2fede93d6b45145e7033306c80b0f009db2
David <davidmollera@hotmail.com>
2022-11-22 21:37:01 -0500

    used the useEffect hook to render in the book imgs, also used the settimeout to give enough time for the loading states to be noticed

--- src/components/ui/book.jsx
+++ src/components/ui/book.jsx
@@ -1,26 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import Rating from "./Rating";
 import Price from "./Price";
 
 const Book = ({ book }) => {
-  function imageLoaded(){
 
-  }
+  const [img, setImg] = useState();
+
+  useEffect(() => {
+    const image = new Image()
+    image.src = book.url
+    image.onload = () => {
+      setTimeout(() => {
+        setImg(image)
+      }, 600);
+    }
+  })
 
   return (
     <div className="book">
-      <div className="book__img--skeleton"></div>
-      <div className="skeleton book__title--skeleton"></div>
-      <div className="skeleton book__rating--skeleton"></div>
-      <div className="skeleton book__price--skeleton"></div>
-      {/* <Link to={`/books/${book.id}`}>
+      {
+        img ? 
+       ( <>
+        <Link to={`/books/${book.id}`}>
         <figure className="book__img--wrapper">
           <img
-            src={book.url}
+            src={img.src}
             alt=""
             className="book__img"
-            onLoad={imageLoaded}
           />
         </figure>
       </Link>
@@ -30,7 +37,17 @@ const Book = ({ book }) => {
         </Link>
       </div>
       <Rating rating={book.rating}/>
-      <Price  salePrice={book.salePrice} originalPrice={book.originalPrice} /> */}
+      <Price  salePrice={book.salePrice} originalPrice={book.originalPrice} />
+      </> )
+      :
+      (<>
+      <div className="book__img--skeleton"></div>
+      <div className="skeleton book__title--skeleton"></div>
+      <div className="skeleton book__rating--skeleton"></div>
+      <div className="skeleton book__price--skeleton"></div>
+      </>)
+      }
+      
     </div>
   );
 };
